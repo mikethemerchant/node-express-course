7766e493b96e4e64d9a9a5cf967f2d0f4dc5f59f
Michael Bender <michael.bender@spartronics.com>
2023-07-14 11:46:59 -0400

    448 starting express

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,15 +1,39 @@
 const http = require('http');
+const {readFileSync} = require('fs');
+
+// get all files
+const homePage = readFileSync('./navbar-app/index.html');
+const homeStyle = readFileSync('./navbar-app/styles.css');
+const homeImage = readFileSync('./navbar-app/logo.svg');
+const homeLogic = readFileSync('./navbar-app/browser-app.js');
 
 const server = http.createServer((req,res)=>{
-    //console.log(req.method);
-    console.log(req/url);
-    res.writeHead(200,{'content-type':'text/html'});
-    res.write('<h1> Home Page </h2>');
-    res.end();
+    const url = req.url;
+    if(url === '/') {
+        res.writeHead(200,{'content-type':'text/html'});
+        res.write(homePage);
+        res.end();
+    } else if (url === '/about' ) {
+        res.writeHead(200,{'content-type':'text/html'});
+        res.write('<h1> about Page </h2>');
+        res.end();
+    } else if (url === '/styles.css' ) {
+        res.writeHead(200,{'content-type':'text/css'});
+        res.write(homeStyle);
+        res.end();
+    } else if (url === '/logo.svg' ) {
+        res.writeHead(200,{'content-type':'image/svg+xml'});
+        res.write(homeImage);
+        res.end();
+    } else if (url === '/browser-app.js' ) {
+        res.writeHead(200,{'content-type':'text/javascript'});
+        res.write(homeLogic);
+        res.end();
+    } else {
+        res.writeHead(404,{'content-type':'text/html'});
+        res.write('<h1> file not found </h2>');
+        res.end();
+    }
 })
 
 server.listen(3000);
-
-// port 80 is http
-// port 443 is https
-// port 22 ssh
