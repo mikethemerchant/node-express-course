7e33b61b263ad7ed0c734313e4d61bfeb2bc8570
Michael Bender <michael.bender@spartronics.com>
2023-07-15 10:12:32 -0400

    json returning block of text 532

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,16 +1,12 @@
 const express = require('express');
-const path = require('path');
 const app = express();
+const {products} = require('./data');
 
-// setup static and middleware
-app.use(express.static('./public'))
-
-app.get('/',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'./navbar-app/index.html'))
+app.get('/', (req, res) => {
+    res.json(products);
 })
 
-app.all('*',(req,res)=>{
-    res.status(404).send('resource not found')
-})
 
-app.listen(3000,()=> {console.log('server running')})
+app.listen(3000, () => {
+    console.log('Server is listening on port 3000....')
+  })
