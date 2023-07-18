0641b09f25ce069694fb49dc62793b1a3b9d17ce
Michael Bender <michael.bender@spartronics.com>
2023-07-18 12:18:26 -0400

    at 534

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,8 +1,17 @@
 const express = require('express')
 const app = express()
 const { products } = require('./data')
+
 app.get('/', (req, res) => {
-  res.json(products)
+  res.send('<h1>Home Page</h1><a href="/api/products">products</a>')
+})
+
+app.get('/api/products', (req, res) => {
+    const newProducts = products.map((product) => {
+        const { id, name, image } = product
+        return { id, name, image }
+    })
+    res.json(newProducts)
 })
 
 app.listen(5000, () => {
