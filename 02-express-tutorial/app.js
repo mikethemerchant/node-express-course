c76d99a661a269bb77fbf411c446bd17f43f030a
mikethemerchant <mikethemerchant@gmail.com>
2023-07-20 10:45:41 -0400

    test push

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -3,7 +3,7 @@ const app = express()
 const { products } = require('./data')
 
 app.get('/', (req, res) => {
-  res.send('<h1>Home Page</h1><a href="/api/products">products</a>')
+  res.send('<h1>hello oliver</h1><a href="/api/products">products</a>')
 })
 
 app.get('/api/products', (req, res) => {
