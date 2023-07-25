f9d1b2c752ef842c4ad04c42a189daccc1132d26
mikethemerchant <mikethemerchant@gmail.com>
2023-07-25 12:05:40 -0400

    750 up to express router

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -41,6 +41,31 @@ app.put('/api/people/:id', (req, res) => {
     res.status(200).json({ success: true, data: newPeople })
 })
 
+app.delete('/api/people/:id', (req, res) => {
+    const person = people.find((person) => person.id === Number(req.params.id))
+
+    if(!person) {
+        return res
+            .status(400)
+            .json({ success: false, msg: `No person with ID:${id}`})
+    } 
+    const newPeople = people.filter((person) => person.id !== Number(req.params.id))
+    return res.status(200).json({ success: true, data: newPeople })
+})
+
+// app.delete('/api/people/:id', (req, res) => {
+//     const person = people.find((person) => person.id === Number(req.params.id))
+//     if (!person) {
+//       return res
+//         .status(404)
+//         .json({ success: false, msg: `no person with id ${req.params.id}` })
+//     }
+//     const newPeople = people.filter(
+//       (person) => person.id !== Number(req.params.id)
+//     )
+//     return res.status(200).json({ success: true, data: newPeople })
+//   })
+
 
 app.post('/api/postman/people', (req, res) => {
     const { name } = req.body
